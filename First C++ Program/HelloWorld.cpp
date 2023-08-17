7efea23752a9f1aac50882d6f188547c997c91a4
harsha201000 <138086385+harsha201000@users.noreply.github.com>
2023-08-17 11:02:03 -0500

    Add files via upload
    My First C++ Program

---
+++ First C++ Program/HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    // Hello World Program in C++
+    std::cout << "Hello World"<< std::endl;
+    std::cout << "Welcome to C++"<< std::endl;
+    std::cout << "Harsha First C++ Program"<< std::endl;
+    std::cout << "My name is Harsha"<< std::endl;
+    std::cout << "I am 13 years old."<< std::endl;
+    std::cout << "My phone number is 6363549059"<< std::endl;
+    std::cout << "My mail id is harsha.malipeddi@gmail.com"<< std::endl;
+    std::cout << "I am in Grade 8"<< std::endl;
+    return 0;
+    
+}
